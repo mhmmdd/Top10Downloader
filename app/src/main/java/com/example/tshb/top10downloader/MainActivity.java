6c03f6a268dd75e9cfd825fdbd7e47b65d6fb117
mhmmdd <mhmmd.dd@gmail.com>
2017-11-05 08:05:25 +0200

    initial commit

---
+++ app/src/main/java/com/example/tshb/top10downloader/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.tshb.top10downloader;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    private class DownloadData extends AsyncTask<String, Void, String> {
+        private static final String TAG = "DownloadData";
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d(TAG, "onPostExecute: parameter is "+s);
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            Log.d(TAG, "doInBackground: starts ");
+            return null;
+        }
+    }
+}
